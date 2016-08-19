77db93bb9e6b4ed7c436b5c555b4b81b6aa20443
Ericzhiluo <eric-luo@live.ca>
2016-08-19 01:58:16 -0400

    refined dashboard using mqtt in client

--- client/views/pages/dashboards/dashboard-2.js
+++ client/views/pages/dashboards/dashboard-2.js
@@ -1,7 +1,9 @@
 Template.dashboard2.rendered = function(){
     // Set white background color for top navbar
     $('body').addClass('light-navbar');
-
+    $('#voltage').html("Loading...");
+    $('#power').html("Loading...");
+    $('#current').html("Loading...")
 };
 
 Template.dashboard2.destroyed = function(){
@@ -11,25 +13,54 @@ Template.dashboard2.destroyed = function(){
 
 import { Machine1 } from '../../../../db/mongo.js';
 
-// Machine1.find({}).observeChanges({
-//   added: function (id, data) {
-//     Template.dashboard2.helpers({
-//       voltage: data.message.d.v12,
-//       power: data.message.d.apower,
-//       current: data.message.d.current
-//     });
-    
-//   }
+var client  = mqtt.connect({
+  // Reads variables from file "development_env.json" located in root
+  // you have to start meteor and load this file using command "meteor --settings development_env.json"
+  host: Meteor.settings.public.host,
+  port: 1883,
+  username: Meteor.settings.public.username,
+  password: Meteor.settings.public.password,
+  clientId: Meteor.settings.public.clientId
+});
+
+client.on('connect', function () {
+  client.subscribe('iot-2/type/Accuvim001/id/+/evt/+/fmt/+');
+  console.log('connected')
+});
+
+client.on('message', Meteor.bindEnvironment(function callback(topic, message) { 
+  console.log(message.toString());
+  parse_message = JSON.parse(message.toString());
+  $('#voltage').html(parse_message.d.v12.toFixed(7))
+  $('#power').html(parse_message.d.apower.toFixed(7))
+  $('#current').html(parse_message.d.current.toFixed(7))
+}));
+
+// Template.dashboard2.helpers({
+//   voltage: function(voltage){
+//     if (typeof voltage !== 'undefined'){
+//       return voltage
+//     } else {
+//       return "loading"
+//     }
+//   },
+//   power: function(voltage){
+//     if (typeof power !== 'undefined'){
+//       return power
+//     } else {
+//       return "loading"
+//     }
+//   },
+//   current: function(voltage){
+//     if (typeof current !== 'undefined'){
+//       return current
+//     } else {
+//       return "loading"
+//     }
+//   },
 // });
 
-Template.dashboard2.helpers({
-  voltage: function() {
-    return Machine1.findOne({}, {sort: {time: -1, limit: 1}}).message.d.v12.toFixed(7);
-  },
-  power: function() {
-    return Machine1.findOne({}, {sort: {time: -1, limit: 1}}).message.d.apower.toFixed(7);
-  },
-  current: function() {
-    return Machine1.findOne({}, {sort: {time: -1, limit: 1}}).message.d.current.toFixed(7);
-  }
-});
+
+
+
+
